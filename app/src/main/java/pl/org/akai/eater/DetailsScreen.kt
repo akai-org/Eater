e31ea0e77acec660a4f4abdc7bc2c58af98c0a8b
jakub <jakub.adamczewski@value-ag.de>
2021-11-14 10:48:29 +0100

    navigation setup

---
+++ app/src/main/java/pl/org/akai/eater/DetailsScreen.kt
@@ -0,0 +1,7 @@
+package pl.org.akai.eater
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen(recipeId: String) {
+}
--- app/src/main/java/pl/org/akai/eater/MainActivity.kt
+++ app/src/main/java/pl/org/akai/eater/MainActivity.kt
@@ -1,12 +1,80 @@
 package pl.org.akai.eater
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import pl.org.akai.eater.navigation.EaterScreen
+import pl.org.akai.eater.util.NullArgumentException
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            EaterApp()
+        }
     }
 }
+
+@Composable
+fun EaterApp() {
+    //TODO add custom EaterTheme
+    MaterialTheme {
+        val allScreens = EaterScreen.values().toList()
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = EaterScreen.fromRoute(backstackEntry.value?.destination?.route)
+
+        Scaffold { innerPadding ->
+            EaterNavHost(
+                navController = navController,
+                Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@Composable
+fun EaterNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = EaterScreen.RecipesList.name,
+        modifier = modifier
+    ) {
+        composable(EaterScreen.RecipesList.name) {
+            ListScreen(
+                onItemClicked = { recipeId ->
+                    navigateToRecipeDetails(navController, recipeId)
+                }
+            )
+        }
+        composable(
+            route = "${EaterScreen.RecipesList.name}/{recipeId}",
+            arguments = listOf(
+                navArgument("recipeId") {
+                    type = NavType.StringType
+                }
+
+            )
+        ) { entry ->
+            val recipeId = entry.arguments?.getString("recipeId")
+            recipeId?.let { DetailsScreen(it)} ?: throw NullArgumentException("recipeId")
+            DetailsScreen(recipeId)
+        }
+    }
+}
+
+private fun navigateToRecipeDetails(navController: NavHostController, recipeId: String) =
+    navController.navigate("${EaterScreen.RecipesList.name}/$recipeId")
+
