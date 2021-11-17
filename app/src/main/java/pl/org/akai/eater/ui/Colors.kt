0f28ac1ae8b9ccbe5f51e8225393770e9c605030
MichalxPZ <michauz2001@gmail.com>
2021-11-17 18:30:11 +0100

    add Theme

--- app/src/main/java/pl/org/akai/eater/ui/Colors.kt
+++ app/src/main/java/pl/org/akai/eater/ui/Colors.kt
@@ -1,4 +1,22 @@
 package pl.org.akai.eater.ui
 
-class Colors {
-}
+import androidx.compose.ui.graphics.Color
+
+val Primary001 = Color(0xff7bb9fa)
+val Primary002 = Color(0xff499ff8)
+val Primary003 = Color(0xffd7eafd)
+
+val Success001 = Color(0xff38db81)
+val Success002 = Color(0xff21b965)
+val Success003 = Color(0xff86e9b2)
+
+val Danger001 = Color(0xfff67f7f)
+val Danger002 = Color(0xfff34e4e)
+val Danger003 = Color(0xfffcd9d9)
+
+val Grey001 = Color(0xffd0d0d0)
+val Grey002 = Color(0xffbfbfbf)
+val Grey003 = Color(0xfff1f1f1)
+
+val Black = Color(0xff282828)
+val White = Color.White
